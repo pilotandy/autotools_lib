2f5bf2ed07a272936509c461d7365589ff5d09fc
pilotandy <pilotcraner@gmail.com>
2023-12-13 23:03:23 -0700

    initial

---
+++ src/library.cpp
@@ -0,0 +1,7 @@
+#include "library.h"
+
+int add(int a, int b)
+{
+    std::cout << a << " + " << b << " = ";
+    return a + b;
+}
---
+++ tests/test.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+#include "library.h"
+
+int main()
+{
+    std::cout << add(3, 5) << std::endl;
+    return 0;
+}
