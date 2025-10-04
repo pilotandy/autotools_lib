460a10b6877d153ad22d6271be88d27eb88c9554
pilotandy <pilotcraner@gmail.com>
2025-10-04 10:16:16 -0600

    Makefile name

--- setup-dev.sh
+++ setup-dev.sh
@@ -14,13 +14,13 @@ LIBRARY_NAME="$1";
 EMAIL="$2";
 
 # Change the src files
-sed -i s/'<library>/$LIBRARY_NAME/' src/MakeFile.am
+sed -i s/'<library>/$LIBRARY_NAME/' src/Makefile.am
 mv src/library.h $LIBRARY_NAME.h
 sed -i s/'<library>/$LIBRARY_NAME/' src/library.cpp
 mv src/library.cpp $LIBRARY_NAME.cpp
 
 # Change the tests files
-sed -i s/'<library>/$LIBRARY_NAME/' tests/MakeFile.am
+sed -i s/'<library>/$LIBRARY_NAME/' tests/Makefile.am
 sed -i s/'<library>/$LIBRARY_NAME/' tests/test.cpp
 
 # Change the configure.ac
